796601c1c85851b8318a747c57fdb97caab84923
xavier <melton.xavier98@gmail.com>
2016-08-23 11:08:36 -0700

    Work in class 8/23/16

--- LetterAlphabet/Main.cpp
+++ LetterAlphabet/Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main
+int main()
 {
+	char charA = 0, charB = 0, charC = 0;
 
+	printf("Please list two letters of the alphabet");
+	scanf_s("%c %c", &charA, 1, &charB, 1);
+	getchar();
+	charA = ((charA - 65) % 32 + 65);
+	charB = ((charB - 65) % 32 + 65);
+	charC = ((charA + charB) / 2);
+	printf("The middle letter is: %c", charC);
+	getchar();
 }
