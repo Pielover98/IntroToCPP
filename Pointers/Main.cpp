3eaa60cb3a8a7c825dae2e88915c091de725c15d
xavier <melton.xavier98@gmail.com>
2016-08-30 15:19:22 -0700

    Structs and References as of 3pm

--- Pointers/Main.cpp
+++ Pointers/Main.cpp
@@ -10,3 +10,14 @@
 4.) You can't make a char pointer into a double
 5.) 
 */
+#include <iostream>
+void main()
+{
+	int var = 128256;
+	float far = var;
+	printf("%f \n", far);
+	far = *(float*)&var;
+	printf("%f\n", far);
+	getchar();
+}
+
--- Structs/Main.cpp
+++ Structs/Main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 struct vec2 
 {
@@ -38,5 +39,44 @@ void main()
 	{
 		printPlayer(players[i]);
 	}
+	getchar();
+}
+*/
+
+// After Reference Lecture Problem
+#include <iostream>
+
+struct Dino
+{
+	char name[80];
+	float age;
+	float powerlvl;
+};
+Dino makeDino()
+{
+	Dino dino;
+	dino.age = 100;
+	dino.powerlvl = 9000;
+	printf("What is dino's name?: ");
+	scanf_s("%s", dino.name, 80);
+	getchar();
+	return dino;
+}
+void printDino(Dino dino)
+{
+	printf("Your dino name %s has an age of %f and has a power level %f \n", dino.name, dino.age, dino.powerlvl);
+}
+void modifyDino(Dino &dino)
+{
+	printf("Please enter a new level that you would like your dino to be at : ");
+	scanf_s("%f", &dino.powerlvl);
+	getchar();
+}
+void main()
+{
+	Dino dino = makeDino();
+	printDino(dino);
+	modifyDino(dino);
+	printDino(dino);
 	getchar();
 }
